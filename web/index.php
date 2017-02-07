6b160b849f17e0d7c2fab95746f7077ebe063e97
Jounior Gerardo <g.jounior07@gmail.com>
2017-02-06 20:03:54 -0500

    ~2.0

--- web/index.php
+++ web/index.php
@@ -1,17 +1,23 @@
 <?php
 require_once __DIR__."/../vendor/autoload.php";
 
+use Symfony\Component\HttpFoundation\Request;
+
 $app = new Silex\Application();
+$app["debug"] = true;
+
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path' => __DIR__.'/../views'
-
 ));
 
-$app['debug'] = true;
 
 $app->get("/", function () use ($app) {
-    return $app["twig"]->render("index.html", array("nombre" => "Samuel"));
+    return $app["twig"]->render("index.html.twig");
+});
+
+$app->get('/register', function(Request $request) use ($app){
+	return $app['twig']->render('register.html.twig');
 });
 
 $app->run();
